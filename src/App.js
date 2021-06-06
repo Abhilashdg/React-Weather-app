0196a4e536816290a6e92ed1dd3841f9e13b7cb8
Abhilash D G <dgabhi12@gmail.com>
2021-06-06 19:35:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+
+import './App.css';
+import React, { useState } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+
+const api = {
+  key: "48485cd3bac591d7638f99c66fdb8255",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
+
+function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then(res => res.json())
+        .then(result => {
+          setWeather(result);
+          setQuery('');
+          console.log(result);
+        });
+    }
+  }
+
+  const dateBuilder = (d) => {
+    let months = ["January", "Febrauray", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`
+  }
+  return (
+    <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
+      <main>
+        <div className="search-box">
+          <input type="text"
+            className="search-bar"
+            placeholder="search"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+          ></input>
+        </div>
+        {(typeof weather.main != "undefined") ? (
+          <div>
+
+            <div className="location-box">{weather.name},{weather.sys.country} </div>
+            <div className="date">{dateBuilder(new Date())}</div>
+
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)} <span>&#176;c</span>
+
+              </div>
+
+            </div>
+            <div className="weather">{weather.weather[0].main}</div>
+            <div className="humi">
+              Humidity: {weather.main.humidity} %
+              </div>
+            <div className="flex">
+              <p className="sunrise-sunset">Sunrise: {new Date(weather.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+              <p className="sunrise-sunset">Sunset: {new Date(weather.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+            </div>
+          </div>
+
+        ) : ('')}
+      </main>
+    </div>
+  );
+}
+
+export default App;
